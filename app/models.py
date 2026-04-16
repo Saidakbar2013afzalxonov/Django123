7c5908622e4c1733bcfafe23e2d322f483d7c189
saidansaf <afzalxonovsaidansaf@gmail.com>
2026-04-16 19:47:13 +0500

    toliq qilindi

---
+++ app/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class User(models.Model):
+    ism=models.CharField(max_length=50,blank=False)
+    familiya=models.CharField(max_length=50,blank=False)
+    yosh=models.PositiveIntegerField(default=15,blank=True)
+
+    def __str__(self):
+        return f"{self.ism} {self.familiya}"
---
+++ app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from . import models
+
+# Create your views here.
+
+def start(request):
+    # users=[
+    #     {'ism':'Saidakbar','familiya':'Afzalxonov','yosh':12}
+    # ]
+
+    users=models.User.objects.all()
+    return render(request, 'app/index.html',context={'users':users})
