168f7c569cf2817665b7aaada4492998a970fb34
saidansaf <afzalxonovsaidansaf@gmail.com>
2026-04-21 20:12:51 +0500

    images

--- app/models.py
+++ app/models.py
@@ -6,6 +6,7 @@ class User(models.Model):
     ism=models.CharField(max_length=50,blank=False)
     familiya=models.CharField(max_length=50,blank=False)
     yosh=models.PositiveIntegerField(default=15,blank=True)
+    picture=models.ImageField(upload_to='images/',default='images/default.jpg',blank=True)
 
     def __str__(self):
         return f"{self.ism} {self.familiya}"
