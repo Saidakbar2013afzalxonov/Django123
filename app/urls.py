3cebb924292f75c37bbd5e52396b8e8a13e41364
saidansaf <afzalxonovsaidansaf@gmail.com>
2026-04-22 21:31:19 +0500

    a taglari

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,9 @@
+
+
 from django.urls import path
 from . import views
 
 urlpatterns=[
-    path("",views.start)
+    path("",views.start,name='user_list'),
+    path("user/<int:user_id>/", views.user_view, name='user_view')
 ]
--- app/views.py
+++ app/views.py
@@ -1,12 +1,14 @@
+
+
 from django.shortcuts import render
 from . import models
 
 # Create your views here.
 
 def start(request):
-    # users=[
-    #     {'ism':'Saidakbar','familiya':'Afzalxonov','yosh':12}
-    # ]
-
     users=models.User.objects.all() 
     return render(request, 'app/index.html',context={'users':users})
+
+def user_view(request,user_id):
+    user=models.User.objects.get(pk=user_id)
+    return render(request,'app/user_view.html',{'user':user})
